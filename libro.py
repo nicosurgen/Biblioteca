a8a06d922650c667f8fa710e98ed383704d95c6f
Nicolás Surgen <132420830+nicosurgen@users.noreply.github.com>
2023-09-11 02:27:16 -0300

    Update libro.py

--- libro.py
+++ libro.py
@@ -1,18 +1,5 @@
 from typing import List,Tuple,Dict
 import random
-"""
-Escribe un programa en el lenguaje de programación que prefieras para gestionar una colección de libros. Debes ser capaz de realizar las siguientes operaciones:
-
-- Agregar un nuevo libro a la colección con información como título, autor, género y número de copias disponibles.
-- Buscar un libro por título o autor y mostrar su información.
-- Prestar un libro a un usuario registrado (reducir el número de copias disponibles).
-- Devolver un libro prestado (aumentar el número de copias disponibles).
-- Registrar usuarios de la biblioteca con información como nombre, número de identificación y libros prestados.
-- Mostrar la información de un usuario, incluyendo los libros que ha prestado.
-- Mostrar una lista de todos los libros en la colección.
-- Mostrar una lista de todos los usuarios registrados en la biblioteca.
-
-"""
 
 def AgregarLibro(Biblioteca:List[Dict]):
     while True:
